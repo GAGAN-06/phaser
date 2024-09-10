67638541a4095c8e061acd312f404d4f081e3c72
Gagandeep <sandhugagandeep0601@gmail.com>
2024-09-10 17:13:40 -0400

    First basic commit nothing much

---
+++ gruntfile.js
@@ -0,0 +1,16 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        connect: {
+            server: {
+                options: {
+                    port: 8000,
+                    base: "src",
+                    keepalive: true,
+                    open: true
+                }
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.registerTask('default', ['connect']);
+};
